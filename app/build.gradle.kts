7367f8c9304259fd759e9d932ac1c1c4c08f3430
mujiaci <1187725424@qq.com>
2026-07-28 21:33:03 +0800

    fix: change lulu1 application id to app.lulu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.jiacimu.lulu"
+        applicationId = "app.lulu"
         minSdk = 26
         targetSdk = 35
         versionCode = 1
