ef2056864a98c7475e89887dff2e9fe48d1832b4
mujiaci <1187725424@qq.com>
2026-07-29 02:12:17 +0800

    refactor: launch consolidated migration host

---
+++ app/src/main/java/com/jiacimu/lulu/MigrationActivity.kt
@@ -0,0 +1,30 @@
+package com.jiacimu.lulu
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.jiacimu.lulu.ai.LuluAiServices
+import com.jiacimu.lulu.data.MigratedDomainStores
+import com.jiacimu.lulu.games.LuluGames
+import com.jiacimu.lulu.study.PostgraduateExamStores
+import com.jiacimu.lulu.study.StarWishStores
+import com.jiacimu.lulu.study.StudyFocusSessions
+import com.jiacimu.lulu.study.StudyRemovedFeatureMigration
+
+class MigrationActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        LuluRepositories.initialize(applicationContext)
+        LuluRepositories.lexicon.initialize(applicationContext)
+        LuluRepositories.worldBook.initialize(applicationContext)
+        MigratedDomainStores.initialize(applicationContext)
+        LuluAiServices.initialize(applicationContext)
+        LuluGames.initialize(applicationContext)
+        StudyRemovedFeatureMigration.migrate(applicationContext)
+        PostgraduateExamStores.initialize(applicationContext)
+        StarWishStores.initialize(applicationContext)
+        StudyFocusSessions.initialize(applicationContext)
+        PostgraduateExamStores.main.syncPomodoroClock()
+        setContent { LuluMigrationRootAppV2() }
+    }
+}
