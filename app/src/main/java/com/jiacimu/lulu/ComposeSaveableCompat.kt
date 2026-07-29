c8dc1af7f10615b6a587cf849aa57879969f7e84
mujiaci <1187725424@qq.com>
2026-07-30 01:54:09 +0800

    fix(compose): support keyed rememberSaveable calls

--- app/src/main/java/com/jiacimu/lulu/ComposeSaveableCompat.kt
+++ app/src/main/java/com/jiacimu/lulu/ComposeSaveableCompat.kt
@@ -6,3 +6,7 @@ import androidx.compose.runtime.Composable
 @Composable
 internal fun <T : Any> rememberSaveable(init: () -> T): T =
     androidx.compose.runtime.saveable.rememberSaveable(init = init)
+
+@Composable
+internal fun <T : Any> rememberSaveable(vararg inputs: Any?, init: () -> T): T =
+    androidx.compose.runtime.saveable.rememberSaveable(*inputs, init = init)
