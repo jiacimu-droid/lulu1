9774c8f4e8a282a557b80f248c1ddf6956b0c0fa
mujiaci <1187725424@qq.com>
2026-08-03 15:35:39 +0800

    Remove temporary BorderStroke compatibility shim

--- app/src/main/java/com/jiacimu/lulu/ComposeAliases.kt
+++
@@ -1,8 +0,0 @@
-package com.jiacimu.lulu
-
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.Dp
-
-/** Package-local helper for call sites that use a solid-color border. */
-fun BorderStroke(width: Dp, color: Color): androidx.compose.foundation.BorderStroke =
-    androidx.compose.foundation.BorderStroke(width, color)
