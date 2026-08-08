40700c45b0dc969b2bfb926ab1c28b66ca1ee123
mujiaci <1187725424@qq.com>
2026-08-08 15:57:12 +0800

    ui: use actionable achievement ordering [skip ci] [skip index]

--- app/src/main/java/com/jiacimu/lulu/study/PostgraduateExamApp.kt
+++ app/src/main/java/com/jiacimu/lulu/study/PostgraduateExamApp.kt
@@ -103,7 +103,7 @@ fun PostgraduateExamApp(
                     StudySection.Plan -> StudyPlanScreenV2(state, store)
                     StudySection.Gacha -> StudyGachaScreen(state, store)
                     StudySection.Collection -> StudyCollectionScreen(state, store, onOpenTheater)
-                    StudySection.Achievements -> StudyAchievementsScreen(state, store)
+                    StudySection.Achievements -> StudyAchievementsScreenV2(state, store)
                     StudySection.Shop -> StudyShopScreen(state, store)
                     StudySection.Guide -> StudyGuideScreen()
                 }
