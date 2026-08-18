8437ae02b47302712f8e00cf3aa32a20fdbc0617
mujiaci <1187725424@qq.com>
2026-08-18 12:12:00 +0800

    refactor: remove obsolete furniture preview compat [skip index]

--- app/src/main/java/com/jiacimu/lulu/DigitalWorldPreviewScaleCompat.kt
+++
@@ -1,6 +0,0 @@
-package com.jiacimu.lulu
-
-import androidx.compose.ui.Modifier
-
-/** Local preview helper; room stickers themselves render at their authored 2D size. */
-internal fun Modifier.graphicsLayer(scaleX: Float, scaleY: Float): Modifier = this
